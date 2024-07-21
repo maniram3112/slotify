7cf233f3412f7f81d4720a7bf2ee509650112178
unknown <maniramthota12@gmail.com>
2024-07-21 22:33:38 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo1 from './logo/logo1.png';
+import logo2 from './logo/logo2.png';
+import logo3 from './logo/logo3.png';
+
+const images = {
+    logo1,
+    logo2,
+    logo3
+}
+
+export default images;
