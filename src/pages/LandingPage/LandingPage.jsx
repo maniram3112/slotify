78ce7e8ffc5e86d750efd559622c64644a0094a5
unknown <maniramthota12@gmail.com>
2024-08-06 22:18:41 +0530

    Basic layout is made

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import images from '../../assets';
+import './LandingPage.css';
+import SignIn from './SignIn/SignIn';
+import SignUp from './SignUp/SignUp';
 
 const LandingPage = () => {
     return (
-        <div>
-            LandingPage
-        </div>
+        // <div className='landingPage'>
+            <div className='pageContainer'>
+                <div className='pageImg'>
+                    <img src={images.landingImg} alt='bankImage'/>
+                </div>
+                <div className='pageAuth'>
+                    {/* <div className='pageAuth-logo'>
+                        <img src={images.logo2} alt='logo'/>
+                    </div> */}
+                    <SignIn/>
+                    <SignUp/>
+                </div>
+            </div>
+        // </div>
     )
 }
 
